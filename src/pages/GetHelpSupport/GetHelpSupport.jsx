b2e675ed646cdb2703408bdb8a9b827007a542e2
amangupta <amangupta3709@gmail.com>
2022-10-30 10:12:29 +0530

    feat: add generic get help support page

---
+++ src/pages/GetHelpSupport/GetHelpSupport.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const GetHelpSupport = () => {
+  return (
+    <div className="GetSupport">
+      <h1>
+        Something went wrong! <br />
+        Try Reloading the page
+      </h1>
+      <button>get help support</button>
+    </div>
+  );
+};
+
+export default GetHelpSupport;
