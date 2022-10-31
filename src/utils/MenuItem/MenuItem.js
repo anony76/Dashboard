393a06e65c95f0e13922181ac1998ee41832cd67
amangupta <amangupta3709@gmail.com>
2022-10-31 17:18:12 +0530

    refactor: add menu items with icons in uitls

---
+++ src/utils/MenuItem/MenuItem.js
@@ -0,0 +1,26 @@
+import StorageIcon from "@mui/icons-material/Storage";
+
+const MenuItem = [
+  {
+    item_name: "Experiment",
+    item_icon: <StorageIcon fontSize="large" />,
+  },
+  {
+    item_name: "Data",
+    item_icon: <StorageIcon fontSize="large" />,
+  },
+  {
+    item_name: "Data Sources",
+    item_icon: <StorageIcon fontSize="large" />,
+  },
+  {
+    item_name: "Model Factory",
+    item_icon: <StorageIcon fontSize="large" />,
+  },
+  {
+    item_name: "Modal Tunning",
+    item_icon: <StorageIcon fontSize="large" />,
+  },
+];
+
+export default MenuItem;
