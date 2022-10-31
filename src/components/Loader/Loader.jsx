7d5b7cc654ec81c273beaa60319463cc10a4d822
amangupta <amangupta3709@gmail.com>
2022-10-31 17:11:24 +0530

    feat: add generic loader component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./Loader.scss";
 const Loader = () => {
   return (
     <div className="Loader">
-      <h1>Loading profile...</h1>
+      <h1>Loading...</h1>
     </div>
   );
 };
