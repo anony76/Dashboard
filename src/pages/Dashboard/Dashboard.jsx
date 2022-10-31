ab79171a6e98c0b4c6fed4aacc42efbd74f03dc8
amangupta <amangupta3709@gmail.com>
2022-10-31 17:15:52 +0530

    refactor: add updated dasboard page with component integration

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+/* packages import */
+import React, { useState } from "react";
+
+/* Components import */
+import { GenericTable, SideMenu } from "../../components";
+
+/* Layouts import */
 import { DashboardLayout } from "../../layout";
 
+/* Style import */
+import "./Dashboard.scss";
+
+/* Utils import */
+import { MenuItem } from "../../utils";
+
 const Dashboard = () => {
+  const [open, setOpen] = useState(true);
+
+  const handleDrawerClose = () => {
+    setOpen(!open);
+  };
   return (
-    <DashboardLayout>
-      <h1>This is Dashboard Page</h1>
+    <DashboardLayout setOpen={setOpen} open={open}>
+      <div className="Dashboard">
+        {open && (
+          <div className="Dashboard--SideMenu">
+            <SideMenu
+              MenuItem={MenuItem}
+              isOpen={open}
+              handleDrawerClose={handleDrawerClose}
+            />
+          </div>
+        )}
+        <div className="Dashboard--Table">
+          <GenericTable />
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
