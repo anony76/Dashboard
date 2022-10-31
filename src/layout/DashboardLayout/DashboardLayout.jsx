89db223f11f125f73f53f3aee06dfe8b01dc1946
amangupta <amangupta3709@gmail.com>
2022-10-31 17:15:32 +0530

    refactor: add updated dasboard layout with component integration

--- src/layout/DashboardLayout/DashboardLayout.jsx
+++ src/layout/DashboardLayout/DashboardLayout.jsx
@@ -1,8 +1,38 @@
+/* Packages import */
 import React from "react";
 
+/* Components import */
+import { NavBar, ProfileCard, SearchBar } from "../../components";
+import { Divider } from "@mui/material";
+
+/* Style import */
+import "./DashboardLayout.scss";
+
 const DashboardLayout = (props) => {
+  const { open, setOpen } = props;
+
+  const handleMenu = () => {
+    setOpen(!open);
+  };
+
   const { children } = props;
-  return <div className="DashboardLayout">{children}</div>;
+  return (
+    <div className="DashboardLayout">
+      <div className="DashboardLayout--NavBar">
+        <NavBar handleMenu={handleMenu} />
+      </div>
+      <Divider />
+      <div className="DashboardLayout--Header">
+        <div className="DashboardLayout--Header--Left">
+          <SearchBar />
+        </div>
+        <div className="DashboardLayout--Header--Right">
+          <ProfileCard />
+        </div>
+      </div>
+      <div className="DashboardLayout--Body">{children}</div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
